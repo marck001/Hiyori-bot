b52012d02028c17d9ab270e69b3524c7cf309e8f
Mac <margeo124@gmail.com>
2025-04-05 21:49:12 -0500

    Increase connection pool size

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(process.env.DB_NAME,  process.env.DB_USER,proces
   dialect: 'mysql',
   port:  process.env.DB_PORT || 3306,
   logging: false,
+  pool: {
+    max: 10, 
+    min: 0,  
+    acquire: 30000, 
+    idle: 10000 
+  }
 });
 
 
